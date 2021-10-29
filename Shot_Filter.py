18318b5949d28979ac75bb938a6537fed1284d0a
samyuktsriram <93282154+samyuktsriram@users.noreply.github.com>
2021-10-29 23:00:32 +0530

    Add files via upload

---
+++ Shot_Filter.py
@@ -0,0 +1,20 @@
+from numpy import NaN
+import pandas
+from pandas.core.frame import DataFrame
+import Shot_Seeker
+
+def Shot_Filter(file):
+    '''to filter out the shots after the 80th minute'''
+
+
+    df = pandas.read_json(file)
+
+    required_shots = Shot_Seeker(file)
+    shots_80 = []
+
+    for i in required_shots:
+        if df.loc[i, 'minute'] > 80:
+            shots_80.append(df.loc[i, 'shot'])
+    return shots_80
+
+#Shot_Filter(file)
