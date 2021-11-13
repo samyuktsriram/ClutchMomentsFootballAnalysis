b2e37d9118f2b30cc7469886855db46e8385b3c3
samyuktsriram <93282154+samyuktsriram@users.noreply.github.com>
2021-11-13 11:44:02 +0530

    Add files via upload

--- Event_Reporter.py
+++ Event_Reporter.py
@@ -4,12 +4,15 @@ from pandas.core.frame import DataFrame
 import Shot_Seeker
 import Shot_Filter
 import Score_Counter
+import socplot
 
 file = '/Users/sam/VSCode/Rough/data1.json'
 
 def Event_Reporter(file, team1, team2):
 
     '''Presents a list of 'clutch' events and their impact on the score'''
+
+    pitch = socplot.Pitch()
 
     df = pandas.read_json(file)
     score = Score_Counter.Score_Counter(file, 80)
@@ -22,25 +25,66 @@ def Event_Reporter(file, team1, team2):
 
     Score1 = score[team1]
     Score2 = score[team2]
-
+    #The following code extracts information about the shots and their outcomes, and prints them
     for i in events:
         #This piece of code looks for goals
         if (df.loc[i, 'shot']['outcome']['id']) == 97: #this is the id for goal
             if (df.loc[i, 'possession_team']['name']) == TeamA:
-                print(TeamA, 'scored a goal!')
+                if 'deflected' in  df.loc[i, 'shot']:
+                    if df.loc[i, 'shot']['deflected'] == True:
+                        print(TeamA, 'scored a goal with a deflected shot!')        
+                else: print(TeamA, 'scored a goal!')
                 Score1 = Score1 + 1
+                x = ((df.loc[i,'location'][0]), 80 - (df.loc[i,'location'][1]))
+                y = ((df.loc[i, 'shot']['end_location'][0]), 80 - (df.loc[i, 'shot']['end_location'][1]))
+                pitch.plot_pass(x,y)
             elif (df.loc[i, 'possession_team']['name']) == TeamB:
                 Score2 = Score2 + 1
-                print(TeamB, 'scored a goal!')
+                if 'deflected' in  df.loc[i, 'shot']:
+                    if df.loc[i, 'shot']['deflected'] == True:
+                        print(TeamB, 'scored a goal with a deflected shot!')        
+                else: print(TeamB, 'scored a goal!')
+                x = ((df.loc[i,'location'][0]), 80 -(df.loc[i,'location'][1]))
+                y = ((df.loc[i, 'shot']['end_location'][0]), 80 -(df.loc[i, 'shot']['end_location'][1]))
+                pitch.plot_pass(x,y, color = 'red')
         
+        if (df.loc[i, 'shot']['outcome']['id']) != 97:
+            if 'one_on_one' in df.loc[i, 'shot']:
+                if df.loc[i, 'shot']['one_on_one'] == True:
+                    if (df.loc[i, 'possession_team']['name']) == TeamA:
+                        print(TeamA, 'missed a one-on-one!')
+                    elif (df.loc[i, 'possession_team']['name']) == TeamB:
+                        print(TeamB, 'missed a one-on-one!')
 
+        if df.loc[i, 'shot']['outcome']['name'] == 'Saved':
+            if (df.loc[i, 'possession_team']['name']) == TeamA:
+                print(TeamA, 'had a shot saved!')
+            elif (df.loc[i, 'possession_team']['name']) == TeamB:
+                print(TeamB, 'had a shot saved!')
+        
+        if df.loc[i, 'shot']['outcome']['name'] == 'Off T':
+            if (df.loc[i, 'possession_team']['name']) == TeamA:
+                print(TeamA, 'took a shot, but it was off target.')
+            elif (df.loc[i, 'possession_team']['name']) == TeamB:
+                print(TeamB, 'took a shot, but it was off target.')
 
-        #elif (df.loc[i, 'shot']['outcome']['id']) == 97: #Look for the code for one-on-ones
+        if df.loc[i, 'shot']['outcome']['name'] == 'Blocked':
+            if (df.loc[i, 'possession_team']['name']) == TeamA:
+                print(TeamA, 'took a shot, but it was blocked.')
+            elif (df.loc[i, 'possession_team']['name']) == TeamB:
+                print(TeamB, 'took a shot, but it was blocked.')
+
+        if df.loc[i, 'shot']['outcome']['name'] == 'Post':
+            if (df.loc[i, 'possession_team']['name']) == TeamA:
+                print(TeamA, 'took a shot, but it hit the post!')
+            elif (df.loc[i, 'possession_team']['name']) == TeamB:
+                print(TeamB, 'took a shot, but it hit the post!')
     
     score_final = Score_Counter.Score_Counter(file, 150) #150 is just a really big number, no football games go on that long
     print('The final score was' , score_final)
+    pitch.show()
 
 
 
 
-#Event_Reporter(file, 'Barcelona', 'Real Sociedad')
+Event_Reporter(file, 'Barcelona', 'Real Sociedad')
